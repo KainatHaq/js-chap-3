331a7752f05f9f8b9f189bad813a58055cbed702
KainatHaq <kainathaq52@gmail.com>
2024-08-11 16:36:42 +0500

    Delete chap 3 directory

--- chap 3/script.js
+++
@@ -1,8 +0,0 @@
-//alert ("I am 20 Years old");
-
-
-//alert ("You have visited this site 14 times");
-
-var content = 'John Doe ordered 5 T-shirt(s) on XYZ clothing store';
-document.write(content);
-console.log(content);
